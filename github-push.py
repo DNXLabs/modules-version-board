b3338dba30eca925fb67aa3ec301a6d23d33cc15
Arthur Diniz <arthurbdiniz@gmail.com>
2020-06-16 20:35:37 -0300

    Adds auto bump API version
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- github-push.py
+++ github-push.py
@@ -9,20 +9,36 @@ GITHUB_REPOSITORY_ID = os.environ['GITHUB_REPOSITORY_ID']
 with open('index.html') as f:
     index_file = f.read()
 
+with open('api.json') as f:
+    api_file = f.read()
+
 # Connect to GitHub API and push the changes.
 github = github3.login(token=GITHUB_TOKEN)
 repository = github.repository_with_id(GITHUB_REPOSITORY_ID)
 
 github_index = repository.file_contents('/index.html', ref=MAIN_BRANCH)
+github_api = repository.file_contents('/api.json', ref=MAIN_BRANCH)
 
-pushed_change = github_index.update(
-    'Bump version',
+pushed_index_change = github_index.update(
+    'Bump front version',
     index_file.encode('utf-8'),
     branch=MAIN_BRANCH
 )
 
 print("Pushed commit {} to {} branch:\n    {}".format(
-    pushed_change['commit'].sha,
+    pushed_index_change['commit'].sha,
     MAIN_BRANCH,
-    pushed_change['commit'].message,
+    pushed_index_change['commit'].message,
+))
+
+pushed_api_change = github_api.update(
+    'Bump API version',
+    api_file.encode('utf-8'),
+    branch=MAIN_BRANCH
+)
+
+print("Pushed commit {} to {} branch:\n    {}".format(
+    pushed_api_change['commit'].sha,
+    MAIN_BRANCH,
+    pushed_api_change['commit'].message,
 ))
